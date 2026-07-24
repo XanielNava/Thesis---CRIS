d32b047cbd9324761660b28990d85304f7b99197
XanielNava <xanielmariosangelo.nava@wvsu.edu.ph>
2026-07-24 23:03:01 +0800

    Gin push ko lang, wala di may nakayo kay basi mas magubas sa local history ko

---
+++ .history/assets/js/pho-dash-firebase_20260724222920.js
@@ -0,0 +1,45 @@
+// js/settings-firebase.js
+
+// 1. Direct CDN imports inside this module (Added setDoc and deleteDoc)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { 
+    getFirestore, 
+    collection, 
+    addDoc, 
+    writeBatch, 
+    doc, 
+    getDocs, 
+    query, 
+    orderBy, 
+    onSnapshot,
+    setDoc,
+    deleteDoc
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+// 2. Your Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBfqjfJoGz591aI8TJjhIS3T4OEvQxX11Y",
+  authDomain: "cris-database-da989.firebaseapp.com",
+  databaseURL: "https://cris-database-da989-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "cris-database-da989",
+  storageBucket: "cris-database-da989.firebasestorage.app",
+  messagingSenderId: "627885439681",
+  appId: "1:627885439681:web:3c657d64c0aad9b4913240",
+  measurementId: "G-0X99BH7GW4"
+};
+
+// 3. Initialize Firebase & Firestore
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// 4. Export them for settings.js, upload scripts, and views
+export { db, collection, addDoc, 
+    writeBatch, 
+    doc, 
+    getDocs, 
+    query, 
+    orderBy, 
+    onSnapshot,
+    setDoc,
+    deleteDoc
+};
