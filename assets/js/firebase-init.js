f0309309fff107751659eaa7b03a206ee6d3aee9
Kunkun <kirkjohnray.menez@wvsu.edu.ph>
2026-07-27 08:59:11 +0800

    Basta

---
+++ assets/js/firebase-init.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+
+const firebaseConfig = { /* your firebase config */ };
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+
+const host = window.location.hostname;
+connectFirestoreEmulator(db, host, 8080);
+connectAuthEmulator(auth, `http://${host}:9099`, { disableWarnings: true });
